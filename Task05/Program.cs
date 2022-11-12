96259a26fb90d6e130303628e38ca055d9884bc4
Valiantsina <valentinass@yandex.ru>
2022-11-12 23:35:29 +0300

    Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число N ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -n;
+while(count <= n)
+{
+    Console.Write ($"{count} ");
+    count = count + 1;
+}
+
+if(n < 0)
+    while(count >= n)
+    {
+    Console.Write ($"{count} ");
+    count = count - 1;
+    }
