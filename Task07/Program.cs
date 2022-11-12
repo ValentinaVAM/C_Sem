0a38c9431a7ae4a71d2a668ec4488d4986ca8648
Valiantsina <valentinass@yandex.ru>
2022-11-12 23:56:00 +0300

    Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 1000 && number > 99)
+{
+    number = number %10;
+    Console.WriteLine($"Последняя цифра {number}");
+}
+else
+{
+    Console.WriteLine("Введите трёхзначное число ");
+}
