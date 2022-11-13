bb9cc8a3b23609b18d776a5b57149dfb327170b9
Valiantsina <valentinass@yandex.ru>
2022-11-13 22:18:47 +0300

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите два числа: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if(a >= b)
+{
+    max = a;
+    Console.WriteLine($"Максимальное число {max}");
+}
+else
+{
+    max = b;
+    Console.WriteLine($"Максимальное число {max}");
+}
