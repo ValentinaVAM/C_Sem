f96002426a007cdc951bbe0d235ea14998c661c0
Valiantsina <valentinass@yandex.ru>
2022-11-19 15:50:30 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Middle(int num)
+{
+    int middleDigit = num / 10;
+    int result = middleDigit % 10;
+    return result;
+}
+
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number < 1000 && number > 99 || number > -1000 && number < -99)
+{
+    int res = Middle(number);
+    Console.WriteLine(res);
+}
+else Console.WriteLine("Число не является трёхзначным");
