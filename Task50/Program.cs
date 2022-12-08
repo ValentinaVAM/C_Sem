2832ce56c74271c9a9e8de87072462eab5f26817
Valiantsina <valentinass@yandex.ru>
2022-12-08 20:48:45 +0300

    Task50

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -7,9 +7,9 @@
 // 1,7 -> такого элемента в массиве нет
 
 
-int[,] CreateMatrixRndInt (int rows, int columns, int min, int max)
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
-    int[,] matrix = new int[rows, columns]; 
+    int[,] matrix = new int[rows, columns];
     Random rnd = new Random();
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -28,8 +28,8 @@ void PrintMatrix(int[,] matrix)
         Console.Write("[");
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
-            else Console.Write($"{matrix[i, j], 4} ");
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
         }
         Console.WriteLine("]");
     }
