5abe74122780c47acafae17afb4c3e40ee24aa04
Valiantsina <valentinass@yandex.ru>
2022-12-09 21:53:07 +0300

    Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int SumNums (int n)
+{
+    if (n == 0) return 0;
+    else
+        return n % 10 + SumNums(n / 10);
+}
+
+int result = SumNums(num);       
+Console.WriteLine(result);    
